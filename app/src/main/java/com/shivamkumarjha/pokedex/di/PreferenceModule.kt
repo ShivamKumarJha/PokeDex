9d8cf5acb94ab1d8df03ef42cf6a6edd5be23b6b
ShivamKumarJha <jha.shivam3@gmail.com>
2021-07-10 14:56:45 +0530

    Hilt DI: Add PreferenceModule
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

---
+++ app/src/main/java/com/shivamkumarjha/pokedex/di/PreferenceModule.kt
@@ -0,0 +1,19 @@
+package com.shivamkumarjha.pokedex.di
+
+import android.content.Context
+import com.shivamkumarjha.pokedex.persistence.PreferenceManager
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class PreferenceModule {
+
+    @Provides
+    @Singleton
+    fun getPreferenceManager(@ApplicationContext context: Context) = PreferenceManager(context)
+}
