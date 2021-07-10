48502c5d6f030b4c51e1db4cc4de2477e6e9747d
ShivamKumarJha <jha.shivam3@gmail.com>
2021-07-10 16:04:44 +0530

    Model: Nullables
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- app/src/main/java/com/shivamkumarjha/pokedex/model/PropertyIII.kt
+++ app/src/main/java/com/shivamkumarjha/pokedex/model/PropertyIII.kt
@@ -4,5 +4,5 @@ import com.google.gson.annotations.SerializedName
 
 data class PropertyIII(
     @SerializedName("front_default") val front_default: String,
-    @SerializedName("front_female") val front_female: String
+    @SerializedName("front_female") val front_female: String?
 )
--- app/src/main/java/com/shivamkumarjha/pokedex/model/PropertyIV.kt
+++ app/src/main/java/com/shivamkumarjha/pokedex/model/PropertyIV.kt
@@ -4,7 +4,7 @@ import com.google.gson.annotations.SerializedName
 
 data class PropertyIV(
     @SerializedName("front_default") val front_default: String,
-    @SerializedName("front_female") val front_female: String,
+    @SerializedName("front_female") val front_female: String?,
     @SerializedName("front_shiny") val front_shiny: String,
-    @SerializedName("front_shiny_female") val front_shiny_female: String
+    @SerializedName("front_shiny_female") val front_shiny_female: String?
 )
