2a5f8761a49769fb0a35ebf78fa8baef4d869034
ShivamKumarJha <jha.shivam3@gmail.com>
2021-07-10 15:50:19 +0530

    Model: Commonize Property model
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- app/src/main/java/com/shivamkumarjha/pokedex/model/BlackWhite.kt
+++ app/src/main/java/com/shivamkumarjha/pokedex/model/BlackWhite.kt
@@ -3,7 +3,7 @@ package com.shivamkumarjha.pokedex.model
 import com.google.gson.annotations.SerializedName
 
 data class BlackWhite(
-    @SerializedName("animated") val animated: Animated,
+    @SerializedName("animated") val animated: Property,
     @SerializedName("back_default") val back_default: String,
     @SerializedName("back_female") val back_female: String,
     @SerializedName("back_shiny") val back_shiny: String,
--- app/src/main/java/com/shivamkumarjha/pokedex/model/GenerationIV.kt
+++ app/src/main/java/com/shivamkumarjha/pokedex/model/GenerationIV.kt
@@ -3,7 +3,7 @@ package com.shivamkumarjha.pokedex.model
 import com.google.gson.annotations.SerializedName
 
 data class GenerationIV(
-    @SerializedName("diamond-pearl") val diamondPearl: DiamondPearl,
-    @SerializedName("heartgold-soulsilver") val heartgoldSoulsilver: HeartGoldSoulSilver,
-    @SerializedName("platinum") val platinum: Platinum
+    @SerializedName("diamond-pearl") val diamondPearl: Property,
+    @SerializedName("heartgold-soulsilver") val heartgoldSoulsilver: Property,
+    @SerializedName("platinum") val platinum: Property
 )
