78cf63a95f7b89fab92838157b51d88cc045b9d9
ShivamKumarJha <jha.shivam3@gmail.com>
2021-07-10 14:47:45 +0530

    Add Constants
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

---
+++ app/src/main/java/com/shivamkumarjha/pokedex/config/Constants.kt
@@ -0,0 +1,14 @@
+package com.shivamkumarjha.pokedex.config
+
+object Constants {
+    const val TAG = "PokeDex"
+
+    //API
+    const val POKEAPI = "https://pokeapi.co/api/v2/"
+
+    //Database
+    const val DB_NAME = "pokedex.db"
+
+    //Preferences
+    const val PREF_NAME = "preferences"
+}
