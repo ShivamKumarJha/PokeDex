5f073a94b9ca3fe0c82b125f257aed2a51e2b819
ShivamKumarJha <jha.shivam3@gmail.com>
2021-07-11 00:03:29 +0530

    Add StethoInterceptor
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- app/src/main/java/com/shivamkumarjha/pokedex/ui/BaseApplication.kt
+++ app/src/main/java/com/shivamkumarjha/pokedex/ui/BaseApplication.kt
@@ -1,7 +1,16 @@
 package com.shivamkumarjha.pokedex.ui
 
 import android.app.Application
+import com.facebook.stetho.Stetho
+import com.shivamkumarjha.pokedex.BuildConfig
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class BaseApplication : Application()
+class BaseApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            Stetho.initializeWithDefaults(applicationContext)
+        }
+    }
+}
