af86df35a7827c9b1e9d61d36315a1180eba3c96
ShivamKumarJha <jha.shivam3@gmail.com>
2021-07-11 12:55:23 +0530

    Refactor PokemonUtility
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- app/src/main/java/com/shivamkumarjha/pokedex/utility/PokemonUtility.kt
+++ app/src/main/java/com/shivamkumarjha/pokedex/utility/PokemonUtility.kt
@@ -1,5 +1,7 @@
 package com.shivamkumarjha.pokedex.utility
 
+import android.util.Log
+import com.shivamkumarjha.pokedex.config.Constants
 import com.shivamkumarjha.pokedex.model.PokemonData
 import com.shivamkumarjha.pokedex.model.Result
 
@@ -9,21 +11,24 @@ object PokemonUtility {
         return PokemonData(
             getId(result.url),
             result.name,
-            getImageUrl(result.url)
+            getImageUrl(getId(result.url))
         )
     }
 
     private fun getId(apiPath: String): Int {
-        return if (apiPath.endsWith("/")) {
-            apiPath.dropLast(1).takeLastWhile { it.isDigit() }
-        } else {
-            apiPath.takeLastWhile { it.isDigit() }
-        }.toInt()
+        return try {
+            if (apiPath.endsWith("/")) {
+                apiPath.dropLast(1).takeLastWhile { it.isDigit() }
+            } else {
+                apiPath.takeLastWhile { it.isDigit() }
+            }.toInt()
+        } catch (exception: Exception) {
+            Log.e(Constants.TAG, exception.message ?: "Error at PokemonUtility: getId(${apiPath})")
+            return 0
+        }
     }
 
-    private fun getImageUrl(apiPath: String): String {
-        return "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${
-            getId(apiPath)
-        }.png"
+    private fun getImageUrl(id: Int): String {
+        return "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png"
     }
 }
